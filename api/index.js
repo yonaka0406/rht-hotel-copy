9da6ef4023e723178d573b00d6339c1c81600f7b
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-01-24 13:00:49 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -43,6 +43,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
