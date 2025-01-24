e42b5fa20e9b008008ab89879ea5451e54bc2f43
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-01-24 13:45:45 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,11 +4,17 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,  // Replace with your actual frontend domain
+  methods: 'GET, POST, PUT, DELETE', 
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // API Routes
