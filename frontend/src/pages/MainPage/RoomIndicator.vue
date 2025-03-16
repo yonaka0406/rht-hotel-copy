5f5bbc230a1f6e49c3247ce07b453016de2f4184
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-03-17 08:45:12 +0900

    Update RoomIndicator.vue
    Composition API

--- frontend/src/pages/MainPage/RoomIndicator.vue
+++ frontend/src/pages/MainPage/RoomIndicator.vue
@@ -104,270 +104,238 @@
 
 </template>
 
-<script>
+<script setup>
+  // Vue
   import { ref, computed, watch, onMounted, onUnmounted, onErrorCaptured } from 'vue';
-  import io from 'socket.io-client';
   import { useRouter } from 'vue-router';
-  import { useToast } from 'primevue/usetoast';
-  import { useHotelStore } from '@/composables/useHotelStore';
-  import { useClientStore } from '@/composables/useClientStore';
-  import { useReservationStore } from '@/composables/useReservationStore';  
+  const router = useRouter(); 
+
   import ReservationAddRoom from './components/ReservationAddRoom.vue';
   import ReservationEdit from './ReservationEdit.vue';
+
+  //Websocket
+  import io from 'socket.io-client';
+  const socket = ref(null);
+
+  // Primevue
+  import { useToast } from 'primevue/usetoast';
+  const toast = useToast();
+
+  //Stores
+  import { useHotelStore } from '@/composables/useHotelStore';
+  const { selectedHotel, selectedHotelId, selectedHotelRooms, fetchHotels, fetchHotel } = useHotelStore();
+  import { useClientStore } from '@/composables/useClientStore';
+  const { clients, fetchClients } = useClientStore();
+  import { useReservationStore } from '@/composables/useReservationStore';
+  const { reservedRoomsDayView, fetchReservationsToday } = useReservationStore();
   import { Panel, Card, Drawer, Skeleton, Avatar } from 'primevue';
   import { DatePicker, Button } from 'primevue';
-  
-  export default {  
-    name: "RoomIndicator",
-    components: {  
-      ReservationAddRoom,
-      ReservationEdit,
-      Panel,
-      Card,
-      Drawer,
-      Skeleton,
-      Avatar,
-      DatePicker,
-      Button,
-    },
-    data() {
-      return {
-        
-      };
-    },
-    setup() {
-      const socket = ref(null);
-      const toast = useToast();
-      const router = useRouter(); 
-      const isUpdating = ref(false);
-      const isLoading = ref(false);
-      const { selectedHotel, selectedHotelId, selectedHotelRooms, fetchHotels, fetchHotel } = useHotelStore();
-      const { clients, fetchClients } = useClientStore();
-      const { reservedRoomsDayView, fetchReservationsToday } = useReservationStore();
       
-      const today = new Date();
+  const isUpdating = ref(false);
+  const isLoading = ref(false);
+  const today = new Date();
       
-      const selectedRoomID = ref(null);
-      const selectedReservationID = ref(null);
-      const drawerVisible = ref(false);
-      const hasReservation = ref(false);
+  const selectedRoomID = ref(null);
+  const selectedReservationID = ref(null);
+  const drawerVisible = ref(false);
+  const hasReservation = ref(false);
       
-      // Helper function
-      const formatDate = (date) => {
-        if (!(date instanceof Date) || isNaN(date.getTime())) {
-          console.error("Invalid Date object:", date);
-          throw new Error("The provided input is not a valid Date object:");
-        }
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-        return `${year}-${month}-${day}`;
-      };      
-      const formatDateWithDay = (date) => {
-          const options = { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit' };
-          const parsedDate = new Date(date);
-          return `${parsedDate.toLocaleDateString(undefined, options)}`;
-      };
+  // Helper function
+  const formatDate = (date) => {
+    if (!(date instanceof Date) || isNaN(date.getTime())) {
+      console.error("Invalid Date object:", date);
+      throw new Error("The provided input is not a valid Date object:");
+    }
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };      
+  const formatDateWithDay = (date) => {
+      const options = { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit' };
+      const parsedDate = new Date(date);
+      return `${parsedDate.toLocaleDateString(undefined, options)}`;
+  };
 
-      const selectedDate = ref(new Date());
+  const selectedDate = ref(new Date());
 
-      // Computed
-      const roomGroups = computed(() => {
-        console.log('reservedRoomsDayView:', reservedRoomsDayView.value);
-        console.log('selectedHotelRooms:', selectedHotelRooms.value);
+  // Computed
+  const roomGroups = computed(() => {
+    console.log('reservedRoomsDayView:', reservedRoomsDayView.value);
+    console.log('selectedHotelRooms:', selectedHotelRooms.value);
 
-        const selectedDateObj = new Date(selectedDate.value);
-        if (isNaN(selectedDateObj.getTime())) {
-            console.error("Invalid selectedDate.value:", selectedDate.value);
-            return [];
-        }
+    const selectedDateObj = new Date(selectedDate.value);
+    if (isNaN(selectedDateObj.getTime())) {
+        console.error("Invalid selectedDate.value:", selectedDate.value);
+        return [];
+    }
 
-        const reservedRoomIds = new Set(
-          reservedRoomsDayView.value?.reservations            
-            ?.map((res) => res.room_id) || []
-        );
-        console.log('reservedRoomIds:',reservedRoomIds)
+    const reservedRoomIds = new Set(
+      reservedRoomsDayView.value?.reservations            
+        ?.map((res) => res.room_id) || []
+    );
+    console.log('reservedRoomIds:',reservedRoomIds)
 
-        const checkInToday = reservedRoomsDayView.value?.reservations?.filter((room) => {
-          const checkInDate = new Date(room.check_in);
-          
-          // Ensure checkInDate is a valid Date object
-          if (isNaN(checkInDate.getTime())) {
-            console.warn(`Invalid check_in date: ${room.check_in}`);
-            return false;
-          }
-
-          return formatDate(checkInDate) === formatDate(selectedDateObj) &&
-          room.status !== 'block';
-        }) || [];
-
-        const checkOutToday = reservedRoomsDayView.value?.reservations?.filter((room) => {
-          const checkOutDate = new Date(room.check_out);
-          
-          // Ensure checkOutDate is a valid Date object
-          if (isNaN(checkOutDate.getTime())) {
-            console.warn(`Invalid check_out date: ${room.check_out}`);
-            return false;
-          }
-          
-          return formatDate(checkOutDate) === formatDate(selectedDateObj) &&
-          room.status !== 'block';
-        }) || [];
-
-
-        const occupiedRooms = reservedRoomsDayView.value?.reservations?.filter((room) => {
-          // Ensure check_in date is valid
-          const checkInDate = new Date(room.check_in);
-          if (isNaN(checkInDate.getTime())) {
-            console.warn(`Invalid check_in date: ${room.check_in}`);
-            return false;
-          }
-
-          // Ensure date is valid
-          const checkOutDate = new Date(room.check_out);
-          if (isNaN(checkOutDate.getTime())) {
-            console.warn(`Invalid date: ${room.check_out}`);
-            return false;
-          }          
-
-          return !isNaN(checkInDate.getTime())
-            && !isNaN(checkOutDate.getTime()) 
-            && checkInDate <= selectedDateObj 
-            && checkOutDate > selectedDateObj
-            && room.cancelled === null
-            && room.status !== 'block';
-        }) || [];
-
-        const blockedRooms = reservedRoomsDayView.value?.reservations?.filter((room) => {
-          return room.status === 'block';
-        }) || [];
-
-        const freeRooms = selectedHotelRooms.value?.filter((room) => 
-          room.room_for_sale_idc === true && !reservedRoomIds.has(room.room_id)
-        ) || [];
-
-
-        const result = [
-          { title: '本日チェックイン', rooms: checkInToday, color: 'bg-blue-100' },
-          { title: '本日チェックアウト', rooms: checkOutToday, color: 'bg-green-100' },
-          { title: '滞在', rooms: occupiedRooms, color: 'bg-yellow-100' },
-          { title: '空室', rooms: freeRooms, color: 'bg-gray-100' },
-          { title: '部屋ブロック', rooms: blockedRooms, color: 'bg-red-100' },
-        ];
-
-        console.log('roomGroups:', result);
-        return result;
-
-      });
+    const checkInToday = reservedRoomsDayView.value?.reservations?.filter((room) => {
+      const checkInDate = new Date(room.check_in);
       
-      const openNewReservation = (room) => {
-        selectedDate.value = formatDate(today);
-        selectedRoomID.value = room.room_id;
-        hasReservation.value = false;
-        drawerVisible.value = true;
-      };
-      const openEditReservation = (room) => {        
-        selectedReservationID.value = room.id;
-        selectedRoomID.value = room.room_id;        
-        hasReservation.value = true;
-        drawerVisible.value = true;
-      };  
-      const goToReservation = () => {
-        router.push({ name: 'ReservationEdit', params: { reservation_id: selectedReservationID.value } });
+      // Ensure checkInDate is a valid Date object
+      if (isNaN(checkInDate.getTime())) {
+        console.warn(`Invalid check_in date: ${room.check_in}`);
+        return false;
       }
 
-      onErrorCaptured((err, instance, info) => {
-        console.error('Error captured:', err, instance, info);
-        // You can also prevent the error from propagating further
-        return false; // Prevents error propagation
-      });
+      return formatDate(checkInDate) === formatDate(selectedDateObj) &&
+      room.status !== 'block';
+    }) || [];
 
-      // Mount
-      onMounted(async () => {
-        
-        isLoading.value = true;
-        console.log('onMounted of RoomIndicator', selectedHotelId.value, today);
-        console.log('onMounted of reservedRoomsDayView', reservedRoomsDayView.value);
-        console.log('onMounted of selectedDate', selectedDate.value);        
+    const checkOutToday = reservedRoomsDayView.value?.reservations?.filter((room) => {
+      const checkOutDate = new Date(room.check_out);
+      
+      // Ensure checkOutDate is a valid Date object
+      if (isNaN(checkOutDate.getTime())) {
+        console.warn(`Invalid check_out date: ${room.check_out}`);
+        return false;
+      }
+      
+      return formatDate(checkOutDate) === formatDate(selectedDateObj) &&
+      room.status !== 'block';
+    }) || [];
 
-        // Establish Socket.IO connection
-        socket.value = io(import.meta.env.VITE_BACKEND_URL);
 
-        socket.value.on('connect', () => {
-          console.log('Connected to server');
-        });
-        socket.value.on('connect_error', (err) => {
-          console.error('Socket connection error:', err);
-        });
-        socket.value.on('connect_timeout', () => {
-          console.error('Socket connection timeout');
-        });
-        
-        socket.value.on('tableUpdate', async (data) => {
-          // Prevent fetching if bulk update is in progress
-          if (isUpdating.value) {
-              console.log('Skipping fetchReservation because update is still running');
-              return;
-          }
+    const occupiedRooms = reservedRoomsDayView.value?.reservations?.filter((room) => {
+      // Ensure check_in date is valid
+      const checkInDate = new Date(room.check_in);
+      if (isNaN(checkInDate.getTime())) {
+        console.warn(`Invalid check_in date: ${room.check_in}`);
+        return false;
+      }
 
-          await fetchReservationsToday(selectedHotelId, today);
-        });
+      // Ensure date is valid
+      const checkOutDate = new Date(room.check_out);
+      if (isNaN(checkOutDate.getTime())) {
+        console.warn(`Invalid date: ${room.check_out}`);
+        return false;
+      }          
 
-        await fetchHotels();
-        await fetchHotel();
+      return !isNaN(checkInDate.getTime())
+        && !isNaN(checkOutDate.getTime()) 
+        && checkInDate <= selectedDateObj 
+        && checkOutDate > selectedDateObj
+        && room.cancelled === null
+        && room.status !== 'block';
+    }) || [];
 
-        await fetchReservationsToday(selectedHotelId.value, formatDate(selectedDate.value));
-        
-        isLoading.value = false;        
-        
-      });
+    const blockedRooms = reservedRoomsDayView.value?.reservations?.filter((room) => {
+      return room.status === 'block';
+    }) || [];
 
-      onUnmounted(() => {
-        // Close the Socket.IO connection when the component is unmounted
-        if (socket.value) {
-          console.log('Disconnected from the server.');
-          socket.value.disconnect();
-        }
-      });
+    const freeRooms = selectedHotelRooms.value?.filter((room) => 
+      room.room_for_sale_idc === true && !reservedRoomIds.has(room.room_id)
+    ) || [];
 
-      // Watch      
-      watch(selectedHotelId, async (newValue, oldValue) => {            
-        try {
-          if (newValue !== oldValue) {
-            selectedDate.value = today;
-            await fetchHotel();
-            await fetchReservationsToday(selectedHotelId.value, formatDate(today));
-          }
-        } catch (error) {
-          console.error('Error in selectedHotelId watcher:', error);
-        }
-      });
-      watch(selectedDate, async (newValue, oldValue) => {
-            if (newValue !== oldValue) {
-              // console.log('selectedDate changed to:',newValue);
-              await fetchReservationsToday(selectedHotelId.value, formatDate(selectedDate.value));
-            }
-      }, { deep: true });
-      watch(reservedRoomsDayView, async (newValue, oldValue) => {
-            if (newValue !== oldValue) {
-              // console.log('reservedRoomsDayView changed to:',newValue);              
-            }
-      }, { deep: true });      
 
-      return {        
-        isLoading,        
-        selectedDate,
-        selectedRoomID,
-        selectedReservationID,
-        drawerVisible,
-        hasReservation,
-        roomGroups,
-        openNewReservation,
-        openEditReservation,
-        goToReservation,
-      };
-    },
+    const result = [
+      { title: '本日チェックイン', rooms: checkInToday, color: 'bg-blue-100' },
+      { title: '本日チェックアウト', rooms: checkOutToday, color: 'bg-green-100' },
+      { title: '滞在', rooms: occupiedRooms, color: 'bg-yellow-100' },
+      { title: '空室', rooms: freeRooms, color: 'bg-gray-100' },
+      { title: '部屋ブロック', rooms: blockedRooms, color: 'bg-red-100' },
+    ];
+
+    console.log('roomGroups:', result);
+    return result;
+
+  });
+  
+  const openNewReservation = (room) => {
+    selectedDate.value = formatDate(today);
+    selectedRoomID.value = room.room_id;
+    hasReservation.value = false;
+    drawerVisible.value = true;
+  };
+  const openEditReservation = (room) => {        
+    selectedReservationID.value = room.id;
+    selectedRoomID.value = room.room_id;        
+    hasReservation.value = true;
+    drawerVisible.value = true;
+  };  
+  const goToReservation = () => {
+    router.push({ name: 'ReservationEdit', params: { reservation_id: selectedReservationID.value } });
   }
+
+  onErrorCaptured((err, instance, info) => {
+    console.error('Error captured:', err, instance, info);
+    // You can also prevent the error from propagating further
+    return false; // Prevents error propagation
+  });
+
+  // Mount
+  onMounted(async () => {
+    
+    isLoading.value = true;
+    console.log('onMounted of RoomIndicator', selectedHotelId.value, today);
+    console.log('onMounted of reservedRoomsDayView', reservedRoomsDayView.value);
+    console.log('onMounted of selectedDate', selectedDate.value);        
+
+    // Establish Socket.IO connection
+    socket.value = io(import.meta.env.VITE_BACKEND_URL);
+
+    socket.value.on('connect', () => {
+      console.log('Connected to server');
+    });
+    socket.value.on('connect_error', (err) => {
+      console.error('Socket connection error:', err);
+    });
+    socket.value.on('connect_timeout', () => {
+      console.error('Socket connection timeout');
+    });
+    
+    socket.value.on('tableUpdate', async (data) => {
+      // Prevent fetching if bulk update is in progress
+      if (isUpdating.value) {
+          console.log('Skipping fetchReservation because update is still running');
+          return;
+      }
+
+      await fetchReservationsToday(selectedHotelId, today);
+    });
+
+    await fetchHotels();
+    await fetchHotel();
+
+    await fetchReservationsToday(selectedHotelId.value, formatDate(selectedDate.value));
+    
+    isLoading.value = false;        
+    
+  });
+
+  onUnmounted(() => {
+    // Close the Socket.IO connection when the component is unmounted
+    if (socket.value) {
+      console.log('Disconnected from the server.');
+      socket.value.disconnect();
+    }
+  });
+
+  // Watch      
+  watch(selectedHotelId, async (newValue, oldValue) => {            
+    try {
+      if (newValue !== oldValue) {
+        selectedDate.value = today;
+        await fetchHotel();
+        await fetchReservationsToday(selectedHotelId.value, formatDate(today));
+      }
+    } catch (error) {
+      console.error('Error in selectedHotelId watcher:', error);
+    }
+  });
+  watch(selectedDate, async (newValue, oldValue) => {
+        if (newValue !== oldValue) {
+          // console.log('selectedDate changed to:',newValue);
+          await fetchReservationsToday(selectedHotelId.value, formatDate(selectedDate.value));
+        }
+  }, { deep: true });
 
 </script>
 
