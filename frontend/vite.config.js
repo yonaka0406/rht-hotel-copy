cbde123ab621106fda5d24ee9028e67a0cb23a4a
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-03-18 09:57:21 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,8 +22,8 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../api/public', // Output the build files to the backend's public folder
-    /*outDir: 'dist',*/
+    /*outDir: '../api/public', // backend's public folder*/
+    outDir: 'dist',
     emptyOutDir: true, // Clears the directory before building
     sourcemap: true, // or 'inline' to embed the source map in the output file
   },
