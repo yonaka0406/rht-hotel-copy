e8473f17e3c4bae4d1b87f9d12a73ec64d843a19
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-03-18 14:08:29 +0900

    Revert "Update vite.config.js"
    This reverts commit f16ba63d16d9d8245218b0d2ee37a51cd6bbf903.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,41 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE');
-
-  return {
-    plugins: [vue(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'), // Alias for cleaner imports
-        'vue': 'vue/dist/vue.esm-bundler.js', // Alias for Vue runtime compilation
-      },
-    },
-    server: {
-      proxy: {
-        // Proxy API requests to the Node.js backend in development
-        '/api': {
-          target: env.VITE_BACKEND_URL || 'http://localhost:5000', // Uses env variable
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-    build: {
-      /*outDir: '../api/public', // backend's public folder*/
-      outDir: 'dist',
-      emptyOutDir: true,
-      sourcemap: true, 
-    },
-    define: {
-      'process.env': env,
-    },
-  };
-
-/*
 export default defineConfig({
   plugins: [vue(), tailwindcss(),],
   resolve: {
@@ -54,10 +21,10 @@ export default defineConfig({
       },
     },
   },
-  build: {    
+  build: {
+    /*outDir: '../api/public', // backend's public folder*/
     outDir: 'dist',
     emptyOutDir: true, // Clears the directory before building
     sourcemap: true, // or 'inline' to embed the source map in the output file
   },
-  */
 });
