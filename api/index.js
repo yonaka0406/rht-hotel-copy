3b9215cdccf95fe8690ba16655107e64e89be268
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-04-09 11:17:43 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -88,6 +88,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(express.raw({ type: 'text/xml' }));
 
 // Make config available to route handlers
 app.use((req, res, next) => {
