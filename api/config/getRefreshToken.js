8270c72dad5682be01202297cdede44b192bdbe0
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-04-18 11:08:29 +0900

    Update getRefreshToken.js

--- api/config/getRefreshToken.js
+++ api/config/getRefreshToken.js
@@ -5,6 +5,7 @@ const readline = require('readline').promises;
 
 const credentialsPath = path.join(__dirname, '../config/google_sheets_credentials.json');
 const tokenPath = path.join(__dirname, '../config/refresh_token.json');
+const redirectUri = 'http://localhost:3000'; // Use a consistent redirect URI
 
 async function getAccessToken() {
   try {
@@ -14,17 +15,18 @@ async function getAccessToken() {
     const oAuth2Client = new google.auth.OAuth2(
       key.client_id,
       key.client_secret,
-      key.redirect_uris[3]
+      redirectUri
     );
 
     const authUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline', // Request a refresh token
       scope: ['https://www.googleapis.com/auth/drive.file'],
+      redirect_uri: redirectUri,
     });
 
     console.log('Authorize this app by visiting this url:', authUrl);
     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-    const code = await rl.question('Enter the code from that page here: ');
+    const code = await rl.question('Enter the code from the URL after granting permission: ');
     rl.close();
 
     const tokenResponse = await oAuth2Client.getToken(code);
