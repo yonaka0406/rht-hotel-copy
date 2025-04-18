e37f7bed34eae28d88db3caafaa7157b15960bff
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-04-18 09:02:28 +0900

    Create getRefreshToken.js

---
+++ api/config/getRefreshToken.js
@@ -0,0 +1,38 @@
+const { google } = require('googleapis');
+const fs = require('fs').promises;
+const path = require('path');
+const readline = require('readline').promises;
+
+const credentialsPath = path.join(__dirname, '../config/google_sheets_credentials.json');
+const tokenPath = path.join(__dirname, '../config/refresh_token.json');
+
+async function getAccessToken() {
+  try {
+    const credentials = await fs.readFile(credentialsPath);
+    const key = JSON.parse(credentials).web;
+
+    const oAuth2Client = new google.auth.OAuth2(
+      key.client_id,
+      key.client_secret,
+      key.redirect_uris[0]
+    );
+
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline', // Request a refresh token
+      scope: ['https://www.googleapis.com/auth/drive.file'],
+    });
+
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    const code = await rl.question('Enter the code from that page here: ');
+    rl.close();
+
+    const tokenResponse = await oAuth2Client.getToken(code);
+    await fs.writeFile(tokenPath, JSON.stringify(tokenResponse.tokens));
+    console.log('Token stored to', tokenPath);
+  } catch (error) {
+    console.error('Error getting token:', error);
+  }
+}
+
+getAccessToken();
