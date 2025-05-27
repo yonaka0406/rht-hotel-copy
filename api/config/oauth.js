213755dbd3c31512cc94b1210148ad26930beb1f
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-05-27 14:56:26 +0900

    Update oauth.js

--- api/config/oauth.js
+++ api/config/oauth.js
@@ -1,6 +1,14 @@
 const { google } = require('googleapis');
 require('dotenv').config({ path: './api/.env' });
 
+let googleCallback = null;
+
+if (process.env.NODE_ENV === 'production') {
+  googleCallback = process.env.PROD_GOOGLE_CALLBACK_URL  
+} else {
+  googleCallback = process.env.GOOGLE_CALLBACK_URL  
+}
+
 /**
  * Creates and configures a Google OAuth2 client.
  * @returns {OAuth2Client} Configured Google OAuth2 client
@@ -9,7 +17,7 @@ function getGoogleOAuth2Client() {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/api/auth/google/callback'
+    googleCallback || 'http://localhost:5000/api/auth/google/callback'
   );
   return oauth2Client;
 }
