1c7c69e479731bed27d4cb0c2f30197d1905d7d6
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-05-27 20:27:45 +0900

    pms2 BIG CHANGE!

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+    {
+      name   : 'backend-dev', // Name for your test/development instance
+      script : './api/index.js', // Path to your backend entry point
+      watch  : false, // Optional: set to true to restart on file changes during dev
+      env_development: { // Note: PM2 uses 'env_YOUR_ENV_NAME'
+        "NODE_ENV": "development",
+        "PORT": 5000 // You can specify other env vars here if needed
+      }
+    },
+    {
+      name   : 'backend-prod', // Name for your production instance
+      script : './api/index.js', // Path to your backend entry point
+      watch  : false,
+      env_production: {
+        "NODE_ENV": "production",
+        "PORT": 5000 // Or a different port if your prod runs on another port
+      }
+    }
+  ]
+};
