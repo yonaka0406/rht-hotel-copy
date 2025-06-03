cf359e55e29169c88b2af379b60b48ca64542668
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-06-03 14:46:06 +0900

    tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Crucial for scanning your Vue components and other JS/TS files
+    // If you use Tailwind classes in other directories/files, add those paths here.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
