26db42d8236cc9442cfb4443d8dc587444ffcd25
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-07-09 13:52:07 +0900

    Dark Mode #03

--- frontend/src/pages/MainPage/Dashboard.vue
+++ frontend/src/pages/MainPage/Dashboard.vue
@@ -1,8 +1,8 @@
 <template>
-    <Panel>        
+    <Panel class="bg-white dark:bg-gray-900 dark:text-gray-100 rounded-xl shadow-lg dark:shadow-xl">        
         <template #header>
             <div class="grid grid-cols-2">
-            <p class="text-lg font-bold">ダッシュボード：</p>
+            <p class="text-lg font-bold dark:text-gray-100">ダッシュボード：</p>
             <DatePicker v-model="selectedDate" 
                 :showIcon="true" 
                 iconDisplay="input" 
@@ -10,6 +10,7 @@
                 :selectOtherMonths="true"                 
                 fluid
                 required 
+                class="dark:bg-gray-800 dark:text-gray-100 rounded"
             />
             </div>        
         </template>
@@ -26,64 +27,68 @@
                 :rows="10"
                 dataKey="id"                
                 stripedRows
+                class="bg-white dark:bg-gray-900 dark:text-gray-100 rounded-xl"
             >
                 <template #header>
                     <div class="flex justify-between">
-                        <p class="font-bold text-lg">予約一覧</p>
+                        <p class="font-bold text-lg dark:text-gray-100">予約一覧</p>
                     </div>
                 </template>
-                <template #empty> 指定されている期間中では予約ありません。 </template>
+                <template #empty> <span class="dark:text-gray-400">指定されている期間中では予約ありません。</span> </template>
                 <Column field="status" header="ステータス" style="width:10%">
                     <template #body="slotProps">
                         <div class="flex justify-center items-center">
-                            <span v-if="slotProps.data.status === 'hold'" class="px-2 py-1 rounded-md bg-yellow-200 text-yellow-700"><i class="pi pi-pause" v-tooltip="'保留中'"></i></span>
-                            <span v-if="slotProps.data.status === 'provisory'" class="px-2 py-1 rounded-md bg-cyan-200 text-cyan-700"><i class="pi pi-clock" v-tooltip="'仮予約'"></i></span>
-                            <span v-if="slotProps.data.status === 'confirmed'" class="px-2 py-1 rounded-md bg-sky-200 text-sky-700"><i class="pi pi-check-circle" v-tooltip="'確定'"></i></span>
-                            <span v-if="slotProps.data.status === 'checked_in'" class="px-2 py-1 rounded-md bg-green-200 text-green-700"><i class="pi pi-user" v-tooltip="'滞在中'"></i></span>
-                            <span v-if="slotProps.data.status === 'checked_out'" class="px-2 py-1 rounded-md bg-purple-200 text-purple-700"><i class="pi pi-sign-out" v-tooltip="'アウト'"></i></span>
-                            <span v-if="slotProps.data.status === 'cancelled'" class="px-2 py-1 rounded-md bg-gray-200 text-gray-700"><i class="pi pi-times" v-tooltip="'キャンセル'"></i></span>
+                            <span v-if="slotProps.data.status === 'hold'" class="px-2 py-1 rounded-md bg-yellow-200 text-yellow-700 dark:bg-yellow-800 dark:text-yellow-200"><i class="pi pi-pause" v-tooltip="'保留中'"></i></span>
+                            <span v-if="slotProps.data.status === 'provisory'" class="px-2 py-1 rounded-md bg-cyan-200 text-cyan-700 dark:bg-cyan-800 dark:text-cyan-200"><i class="pi pi-clock" v-tooltip="'仮予約'"></i></span>
+                            <span v-if="slotProps.data.status === 'confirmed'" class="px-2 py-1 rounded-md bg-sky-200 text-sky-700 dark:bg-sky-800 dark:text-sky-200"><i class="pi pi-check-circle" v-tooltip="'確定'"></i></span>
+                            <span v-if="slotProps.data.status === 'checked_in'" class="px-2 py-1 rounded-md bg-green-200 text-green-700 dark:bg-green-800 dark:text-green-200"><i class="pi pi-user" v-tooltip="'滞在中'"></i></span>
+                            <span v-if="slotProps.data.status === 'checked_out'" class="px-2 py-1 rounded-md bg-purple-200 text-purple-700 dark:bg-purple-800 dark:text-purple-200"><i class="pi pi-sign-out" v-tooltip="'アウト'"></i></span>
+                            <span v-if="slotProps.data.status === 'cancelled'" class="px-2 py-1 rounded-md bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-200"><i class="pi pi-times" v-tooltip="'キャンセル'"></i></span>
                         </div>                        
                     </template>
                 </Column>
                 <Column field="booker_name" header="予約者" style="width:20%">
+                    <template #body="slotProps">
+                        <span class="dark:text-gray-100">{{ slotProps.data.booker_name }}</span>
+                    </template>
                 </Column>
                 <Column field="clients_json" header="宿泊者" style="width:20%">
                     <template #body="{ data }">
-                        <span v-if="data.clients_json" v-tooltip="formatClientNames(data.clients_json)" style="white-space: pre-line;">
+                        <span v-if="data.clients_json" v-tooltip="formatClientNames(data.clients_json)" style="white-space: pre-line;" class="dark:text-gray-100">
                             {{ getVisibleClientNames(data.clients_json) }}
                         </span>
                     </template>
                 </Column>
                 <Column field="check_in" header="チェックイン" sortable style="width:15%">
                     <template #body="slotProps">                        
-                        <span>{{ formatDateWithDay(slotProps.data.check_in) }}</span>
+                        <span class="dark:text-gray-100">{{ formatDateWithDay(slotProps.data.check_in) }}</span>
                     </template>                    
                 </Column>
                 <Column field="number_of_people" header="宿泊者数" sortable style="width:10%">
                     <template #body="slotProps">
                         <div class="flex justify-end mr-4">
-                            <span>{{ slotProps.data.number_of_people }}</span>
+                            <span class="dark:text-gray-100">{{ slotProps.data.number_of_people }}</span>
                         </div>
                     </template> 
                 </Column>
                 <Column field="number_of_nights" header="宿泊数" sortable style="width:10%">
                     <template #body="slotProps">
                         <div class="flex justify-end mr-4">
-                            <span>{{ slotProps.data.number_of_nights }}</span>
+                            <span class="dark:text-gray-100">{{ slotProps.data.number_of_nights }}</span>
                         </div>
                     </template> 
                 </Column>                
                 <Column field="price" header="料金" sortable style="width:10%">
                     <template #body="slotProps">
                         <div class="flex justify-end mr-2">
-                            <span class="items-end">{{ formatCurrency(slotProps.data.price) }}</span>
+                            <span class="items-end dark:text-gray-100">{{ formatCurrency(slotProps.data.price) }}</span>
                         </div>                        
                     </template>
                 </Column>                
             </DataTable>
         </div>
         
-        <Drawer v-model:visible="drawerVisible" :modal="true" :closable="true">
+        <Drawer v-model:visible="drawerVisible" :modal="true" :closable="true" class="bg-white dark:bg-gray-900 dark:text-gray-100 rounded-xl">
         </Drawer>
         
     </Panel>    
