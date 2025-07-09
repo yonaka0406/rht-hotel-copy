7e2aea9841ca93d51f142c4453908939bf83d3d2
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-07-09 13:19:34 +0900

    Dark Mode #01

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // Crucial for scanning your Vue components and other JS/TS files
-    // If you use Tailwind classes in other directories/files, add those paths here.
-  ],
-  darkMode: 'media', // or 'class' if you want to toggle manually
+  darkMode: 'media',
   theme: {
     extend: {},
   },
-  plugins: [],
 }
