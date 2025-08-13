e851a6b72969c03309a7b944392e49d34aa5b63d
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-08-13 16:39:05 +0900

    Update hotelsRoutes.js

--- api/routes/hotelsRoutes.js
+++ api/routes/hotelsRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { hotels, roomTypeCreate, roomCreate, getHotels, getHotelRoomTypes, editHotel, editHotelSiteController, editRoomType, editRoom, editHotelCalendar, getHotelRooms, fetchHotelSiteController, getBlockedRooms, editBlockedRooms, getPlanExclusionSettingsController, updatePlanExclusionSettingsController } = require('../controllers/hotelsController');
-const { authMiddleware, authMiddleware_manageDB } = require('../middleware/authMiddleware');
+const { authMiddleware, authMiddlewareCRUDAccess, authMiddleware_manageDB } = require('../middleware/authMiddleware');
 
 router.get('/hotel-list', authMiddleware, getHotels);
 router.get('/hotel-room-types/:id', authMiddleware, getHotelRoomTypes);
@@ -18,8 +18,9 @@ router.put('/hotel/:id', authMiddleware_manageDB, editHotel);
 router.put('/hotel-ota/:id', authMiddleware_manageDB, editHotelSiteController);
 router.put('/room-type/:id', authMiddleware_manageDB, editRoomType);
 router.put('/room/:id', authMiddleware_manageDB, editRoom);
-router.put('/hotel-calendar/update/:startDate/:endDate', authMiddleware_manageDB, editHotelCalendar);
-router.put('/hotel-calendar/unblock/:id', authMiddleware_manageDB, editBlockedRooms);
 router.put('/hotels/:hotel_id/plan-exclusions', authMiddleware_manageDB, updatePlanExclusionSettingsController);
+
+router.put('/hotel-calendar/update/:startDate/:endDate', authMiddlewareCRUDAccess, editHotelCalendar);
+router.put('/hotel-calendar/unblock/:id', authMiddlewareCRUDAccess, editBlockedRooms);
 
 module.exports = router;
