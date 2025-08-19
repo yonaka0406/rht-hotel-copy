596377863bdb0ba966f691e121c5f5e50ff1f4eb
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-08-19 10:58:44 +0900

    Update billing.js

--- api/models/billing.js
+++ api/models/billing.js
@@ -16,7 +16,7 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
         ,reservations.check_out
         ,reservations.check_out - reservations.check_in AS number_of_nights
         ,reservations.number_of_people
-        ,details.plan_price		
+        ,details.plan_price
         ,details.addon_price
         ,(details.plan_price + details.addon_price) AS price
         ,$2 AS period_start
@@ -25,12 +25,22 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
         ,details.addon_period_price
         ,(details.plan_period_price + details.addon_period_price) AS period_price
         ,details.payment
-        ,CASE WHEN (details.plan_price + details.addon_price) > details.payment THEN (details.plan_price + details.addon_price - details.payment) ELSE 0 END AS balance
-		    ,GREATEST(0, LEAST((details.plan_period_price + details.addon_period_price), (details.plan_price + details.addon_price - details.payment))) AS period_payable
+        ,CASE 
+          WHEN (details.plan_price + details.addon_price) > details.payment 
+          THEN (details.plan_price + details.addon_price - details.payment) 
+          ELSE 0 
+        END AS balance
+        ,GREATEST(
+          0, 
+          LEAST(
+            (details.plan_period_price + details.addon_period_price), 
+            (details.plan_price + details.addon_price - details.payment)
+          )
+        ) AS period_payable
         ,details.clients_json
         ,details.payers_json
       FROM
-        reservations	
+        reservations
         ,hotels
         ,clients AS booker
         ,(
@@ -39,38 +49,41 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
             ,reservation_details.reservation_id
             ,rc.clients_json::TEXT AS clients_json
             ,rpc.clients_json::TEXT AS payers_json
-            ,COALESCE(rp.payment,0) as payment
+            ,COALESCE(rp.payment,0) as payment            
             ,SUM(
-              CASE WHEN reservation_details.plan_type = 'per_room' THEN rr.rates_price
-              ELSE rr.rates_price * reservation_details.number_of_people END              
+              CASE WHEN reservation_details.plan_type = 'per_room' 
+                  THEN reservation_details.price
+                  ELSE reservation_details.price * reservation_details.number_of_people 
+              END
             ) AS plan_price
             ,SUM(
-              CASE WHEN reservation_details.billable = TRUE AND reservation_details.cancelled IS NULL THEN COALESCE(ra.addon_sum,0) ELSE 0 END
+              CASE WHEN reservation_details.billable = TRUE 
+                        AND reservation_details.cancelled IS NULL 
+                  THEN COALESCE(ra.addon_sum,0) 
+                  ELSE 0 
+              END
             ) AS addon_price
             ,SUM(
               CASE WHEN reservation_details.date BETWEEN $2 AND $3 THEN
-                CASE WHEN reservation_details.plan_type = 'per_room' THEN rr.rates_price
-                ELSE rr.rates_price * reservation_details.number_of_people END
+                CASE WHEN reservation_details.plan_type = 'per_room' 
+                    THEN reservation_details.price
+                    ELSE reservation_details.price * reservation_details.number_of_people 
+                END
               ELSE 0 END
             ) AS plan_period_price
             ,SUM(
               CASE WHEN reservation_details.date BETWEEN $2 AND $3 THEN
-                CASE WHEN reservation_details.billable = TRUE AND reservation_details.cancelled IS NULL THEN COALESCE(ra.addon_sum,0) ELSE 0 END
+                CASE WHEN reservation_details.billable = TRUE 
+                          AND reservation_details.cancelled IS NULL 
+                    THEN COALESCE(ra.addon_sum,0) 
+                    ELSE 0 
+                END
               ELSE 0 END
-            ) AS addon_period_price 
+            ) AS addon_period_price
           FROM
             reservation_details 
-              LEFT JOIN 
-            (
-              SELECT 
-                rr.reservation_details_id
-                ,SUM(rr.price) AS rates_price
-              FROM reservation_rates rr, reservation_details rd
-              WHERE rr.reservation_details_id = rd.id AND rd.billable = TRUE 
-                      AND (rd.cancelled IS NULL OR rr.adjustment_type = 'base_rate')
-              GROUP BY rr.reservation_details_id
-            ) rr ON rr.reservation_details_id = reservation_details.id           
-              LEFT JOIN
+
+            LEFT JOIN
             (
               SELECT 
                 ra.hotel_id
@@ -78,10 +91,11 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
                 ,SUM(COALESCE(ra.quantity,0) * COALESCE(ra.price,0)) as addon_sum
               FROM reservation_addons ra
               GROUP BY ra.hotel_id, ra.reservation_detail_id
-            )
-            ra
-              ON reservation_details.hotel_id = ra.hotel_id AND reservation_details.id = ra.reservation_detail_id
-              LEFT JOIN 
+            ) ra
+              ON reservation_details.hotel_id = ra.hotel_id 
+            AND reservation_details.id = ra.reservation_detail_id
+
+            LEFT JOIN 
             (
               SELECT 
                 rc.reservation_id,
@@ -96,23 +110,30 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
                   )
                 ) AS clients_json
               FROM 
-                (SELECT DISTINCT reservation_id, client_id FROM reservation_clients rc JOIN reservation_details rd ON rc.reservation_details_id = rd.id) rc
+                (SELECT DISTINCT reservation_id, client_id 
+                FROM reservation_clients rc 
+                JOIN reservation_details rd 
+                  ON rc.reservation_details_id = rd.id
+                ) rc
                 JOIN clients c ON rc.client_id = c.id
               GROUP BY rc.reservation_id
-            ) rc ON rc.reservation_id = reservation_details.reservation_id
-             LEFT JOIN
+            ) rc 
+              ON rc.reservation_id = reservation_details.reservation_id
+
+            LEFT JOIN
             (
               SELECT
                 reservation_id
                 ,SUM(value) as payment
               FROM 
                 reservation_payments 
-                  JOIN 
-                payment_types 
-                  ON reservation_payments.payment_type_id = payment_types.id              
+                JOIN payment_types 
+                  ON reservation_payments.payment_type_id = payment_types.id
               GROUP BY reservation_id
-            ) rp ON rp.reservation_id = reservation_details.reservation_id
-              LEFT JOIN
+            ) rp 
+              ON rp.reservation_id = reservation_details.reservation_id
+
+            LEFT JOIN
             (
               SELECT 
                 rpc.reservation_id,
@@ -127,23 +148,28 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
                   )
                 ) AS clients_json
               FROM 
-                (SELECT DISTINCT reservation_id, client_id FROM reservation_payments rp JOIN reservations r ON rp.reservation_id = r.id) rpc
+                (SELECT DISTINCT reservation_id, client_id 
+                FROM reservation_payments rp 
+                JOIN reservations r ON rp.reservation_id = r.id
+                ) rpc
                 JOIN clients c ON rpc.client_id = c.id
               GROUP BY rpc.reservation_id
-            ) rpc ON rpc.reservation_id = reservation_details.reservation_id
+            ) rpc 
+              ON rpc.reservation_id = reservation_details.reservation_id
+
           WHERE
             reservation_details.hotel_id = $1
           GROUP BY
-            reservation_details.hotel_id
-            ,reservation_details.reservation_id
-            ,rc.clients_json::TEXT
-            ,rpc.clients_json::TEXT          
-            ,rp.payment
+            reservation_details.hotel_id,
+            reservation_details.reservation_id,
+            rc.clients_json::TEXT,
+            rpc.clients_json::TEXT,
+            rp.payment
         ) AS details
       WHERE
         reservations.hotel_id = $1
         AND reservations.check_out > $2
-        AND reservations.check_in <= $3	  
+        AND reservations.check_in <= $3
         AND reservations.status not in ('block', 'hold')
         AND reservations.reservation_client_id = booker.id
         AND reservations.id = details.reservation_id
@@ -152,7 +178,7 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
         AND (details.plan_price + details.addon_price - details.payment) > 0
       ORDER BY 5, 7;
     `;
-    const values = [hotelId, dateStart, dateEnd]
+    const values = [hotelId, dateStart, dateEnd];
 
     try {
       const result = await pool.query(query, values);    
@@ -162,6 +188,7 @@ const selectBillableListView = async (requestId, hotelId, dateStart, dateEnd) =>
       throw new Error('Database error');
     }
 };
+
 const selectBilledListView = async (requestId, hotelId, month) => {
   const pool = getPool(requestId);
   const query = `
