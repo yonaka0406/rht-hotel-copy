bf0efada30cbc0179e22f783c8659a6519a75efe
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-09-03 16:56:53 +0900

    Update ClientAutoCompleteWithStore.vue

--- frontend/src/components/ClientAutoCompleteWithStore.vue
+++ frontend/src/components/ClientAutoCompleteWithStore.vue
@@ -29,16 +29,16 @@ const emit = defineEmits(['update:modelValue', 'option-select', 'change', 'clear
 const { clients, clientsIsLoading, fetchClients, setClientsIsLoading } = useClientStore();
 
 watch(() => props.modelValue, (val) => {
-  console.log('[ClientAutoCompleteWithStore] props.modelValue changed:', val);
+  //console.log('[ClientAutoCompleteWithStore] props.modelValue changed:', val);
 });
 
 const selectedClientProxy = computed({
   get: () => {
-    console.log('[ClientAutoCompleteWithStore] selectedClientProxy.get:', props.modelValue);
+    //console.log('[ClientAutoCompleteWithStore] selectedClientProxy.get:', props.modelValue);
     return props.modelValue;
   },
   set: v => {
-    console.log('[ClientAutoCompleteWithStore] selectedClientProxy.set:', v);
+    //console.log('[ClientAutoCompleteWithStore] selectedClientProxy.set:', v);
     emit('update:modelValue', v);
   },
 });
@@ -67,7 +67,7 @@ const ensureDisplayName = (client) => {
 };
 
 const filterClients = (event) => {
-  console.log('[ClientAutoCompleteWithStore] Search/filter event:', event.query);
+  //console.log('[ClientAutoCompleteWithStore] Search/filter event:', event.query);
   const query = event.query.toLowerCase();
   const normalizedQuery = normalizePhone(query);
   const isNumericQuery = /^\d+$/.test(normalizedQuery);
@@ -91,28 +91,28 @@ const filterClients = (event) => {
 };
 
 const handleComplete = (event) => {
-  console.log('[ClientAutoCompleteWithStore] @complete event:', event);
+  //console.log('[ClientAutoCompleteWithStore] @complete event:', event);
   filterClients(event);
 };
 const handleOptionSelect = (event) => {
-  console.log('[ClientAutoCompleteWithStore] @option-select event:', event);
+  //console.log('[ClientAutoCompleteWithStore] @option-select event:', event);
   emit('option-select', event);
 };
 const handleChange = (event) => {
-  console.log('[ClientAutoCompleteWithStore] @change event:', event);
+  //console.log('[ClientAutoCompleteWithStore] @change event:', event);
   emit('change', event);
 };
 const handleClear = (event) => {
-  console.log('[ClientAutoCompleteWithStore] @clear event:', event);
+  //console.log('[ClientAutoCompleteWithStore] @clear event:', event);
   emit('clear', event);
 };
 
 watch(selectedClientProxy, (val) => {
-  console.log('[ClientAutoCompleteWithStore] v-model changed:', val);
+  //console.log('[ClientAutoCompleteWithStore] v-model changed:', val);
 });
 
 onMounted(async () => {
-  console.log('[ClientAutoCompleteWithStore] Loading clients...');
+  //console.log('[ClientAutoCompleteWithStore] Loading clients...');
   if (!clients.value || clients.value.length === 0) {
     setClientsIsLoading(true);
     const clientsTotalPages = await fetchClients(1);
@@ -125,6 +125,6 @@ onMounted(async () => {
   if (clients.value && Array.isArray(clients.value)) {
     clients.value.forEach(ensureDisplayName);
   }
-  console.log('[ClientAutoCompleteWithStore] Clients loaded.');
+  //console.log('[ClientAutoCompleteWithStore] Clients loaded.');
 });
 </script> 
