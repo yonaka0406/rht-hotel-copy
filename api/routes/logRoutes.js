73257bce3bb0f04fd2168c391cc7e33cd3c0da67
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-10-06 14:42:15 +0900

    Refactor log controller file structure
    Renamed logController.js to log/main.js and added an index.js to re-export main. This improves organization and prepares for potential expansion of log-related controllers.

--- api/routes/logRoutes.js
+++ api/routes/logRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { fetchReservationHistory, fetchReservationInventoryChange, fetchClientHistory } = require('../controllers/logController');
+const logController = require('../controllers/log');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
-router.get('/log/reservation/:id', authMiddleware, fetchReservationHistory);
-router.get('/log/client/:id', authMiddleware, fetchClientHistory);
+router.get('/log/reservation/:id', authMiddleware, logController.fetchReservationHistory);
+router.get('/log/client/:id', authMiddleware, logController.fetchClientHistory);
 
 // Internal route
-router.get('/log/reservation-inventory/:id/:type', fetchReservationInventoryChange);
+router.get('/log/reservation-inventory/:id/:type', logController.fetchReservationInventoryChange);
 
 module.exports = router;
