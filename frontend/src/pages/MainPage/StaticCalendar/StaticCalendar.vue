16a514d7c4601b2a21f243d87e0b4c3904f1b9ba
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-10-15 14:12:43 +0900

    Update StaticCalendar.vue

--- frontend/src/pages/MainPage/StaticCalendar/StaticCalendar.vue
+++ frontend/src/pages/MainPage/StaticCalendar/StaticCalendar.vue
@@ -181,21 +181,9 @@ const { reservedRooms, fetchReservedRooms } = useReservationStore();
 import { useParkingStore } from '@/composables/useParkingStore';
 const { fetchReservedParkingSpots, reservedParkingSpots, fetchAllParkingSpotsByHotel } = useParkingStore();
 
-// Helper function
-const formatDate = (date) => {
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error("Invalid Date object provided to formatDate.");
-  }
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-};
-const formatDateWithDay = (date) => {
-  const options = { weekday: 'short', year: '2-digit', month: '2-digit', day: '2-digit' };
-  const parsedDate = new Date(date);
-  return `${parsedDate.toLocaleDateString('ja-JP', options)}`;
-};
+import { formatDate, formatDateWithDay } from '@/utils/dateUtils';
+
+
 
 // Room type color assignment
 const roomTypeGrayTones = [
