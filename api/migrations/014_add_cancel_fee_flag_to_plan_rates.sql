9d87d401de23c00d5bee0fd744aeee2fc8f95a4c
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-11-14 08:52:06 +0900

    schema

--- api/migrations/014_add_cancel_fee_flag_to_plan_rates.sql
+++
@@ -1,8 +0,0 @@
--- Add include_in_cancel_fee to plan_rates table
-ALTER TABLE plan_rates
-ADD COLUMN include_in_cancel_fee BOOLEAN NOT NULL DEFAULT FALSE;
-
--- Batch update existing base_rate entries to set the new flag to TRUE
-UPDATE plan_rates
-SET include_in_cancel_fee = TRUE
-WHERE adjustment_type = 'base_rate';
--- api/migrations/099_temp_add_cancel_fee_flag.sql
+++ api/migrations/099_temp_add_cancel_fee_flag.sql
@@ -1,3 +1,12 @@
+-- Add include_in_cancel_fee to plan_rates table
+ALTER TABLE plan_rates
+ADD COLUMN include_in_cancel_fee BOOLEAN NOT NULL DEFAULT FALSE;
+
+-- Batch update existing base_rate entries to set the new flag to TRUE
+UPDATE plan_rates
+SET include_in_cancel_fee = TRUE
+WHERE adjustment_type = 'base_rate';
+
 -- Add include_in_cancel_fee to reservation_rates table
 ALTER TABLE reservation_rates
 ADD COLUMN include_in_cancel_fee BOOLEAN NOT NULL DEFAULT FALSE;
