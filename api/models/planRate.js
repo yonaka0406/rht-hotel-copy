53c3b4fcf4e2fd119dd9faa556608e1c8853ba2b
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-11-13 10:56:33 +0900

    Update planRate.js

--- api/models/planRate.js
+++ api/models/planRate.js
@@ -74,7 +74,13 @@ let actualIsValidCondition = isValidCondition;
 const getAllPlansRates = async (requestId, plans_global_id, plans_hotel_id, hotel_id) => {
     const pool = actualGetPool(requestId);
     const query = `
-        SELECT * FROM plans_rates
+        SELECT 
+            id, hotel_id, plans_global_id, plans_hotel_id, 
+            adjustment_type, adjustment_value, tax_type_id, tax_rate, 
+            condition_type, condition_value, date_start, date_end, 
+            created_at, created_by, updated_at, updated_by, 
+            include_in_cancel_fee
+        FROM plans_rates
         WHERE 
             (plans_global_id = $1 AND plans_hotel_id IS NULL) OR 
             (plans_hotel_id = $2 AND hotel_id = $3 AND plans_global_id IS NULL)
@@ -97,7 +103,7 @@ const getAllPlansRates = async (requestId, plans_global_id, plans_hotel_id, hote
 // Get plans_rates by ID
 const getPlansRateById = async (requestId, id) => {
     const pool = actualGetPool(requestId);
-    const query = 'SELECT * FROM plans_rates WHERE id = $1';
+    const query = 'SELECT id, hotel_id, plans_global_id, plans_hotel_id, adjustment_type, adjustment_value, tax_type_id, tax_rate, condition_type, condition_value, date_start, date_end, created_at, created_by, updated_at, updated_by, include_in_cancel_fee FROM plans_rates WHERE id = $1';
 
     try {
         const result = await pool.query(query, [id]);
@@ -287,8 +293,9 @@ const createPlansRate = async (requestId, plansRate) => {
             date_start, 
             date_end, 
             created_by,
-            updated_by
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+            updated_by,
+            include_in_cancel_fee
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
         RETURNING *
     `;
 
@@ -305,7 +312,8 @@ const createPlansRate = async (requestId, plansRate) => {
         plansRate.date_start,
         plansRate.date_end,
         plansRate.created_by,
-        plansRate.updated_by
+        plansRate.updated_by,
+        plansRate.include_in_cancel_fee || false
     ];
 
     try {
@@ -334,8 +342,9 @@ const updatePlansRate = async (requestId, id, plansRate) => {
             condition_value = $9,
             date_start = $10,
             date_end = $11,
-            updated_by = $12
-        WHERE id = $13
+            updated_by = $12,
+            include_in_cancel_fee = $13
+        WHERE id = $14
         RETURNING *
     `;
 
@@ -352,6 +361,7 @@ const updatePlansRate = async (requestId, id, plansRate) => {
         plansRate.date_start,
         plansRate.date_end,
         plansRate.updated_by,
+        plansRate.include_in_cancel_fee || false,
         id
     ];
 
