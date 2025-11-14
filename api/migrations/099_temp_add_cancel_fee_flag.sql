3e9b717b2da00164cb816e059e208b3d4e464e51
almeida-rht <almeida.ped@redhorse-group.co.jp>
2025-11-14 10:38:37 +0900

    Update 099_temp_add_cancel_fee_flag.sql

--- api/migrations/099_temp_add_cancel_fee_flag.sql
+++ api/migrations/099_temp_add_cancel_fee_flag.sql
@@ -9,7 +9,7 @@ ADD COLUMN comment TEXT;
 -- Batch update existing base_rate entries to set the new flag to TRUE
 UPDATE plans_rates
 SET include_in_cancel_fee = TRUE
-WHERE adjustment_type = 'base_rate';
+WHERE adjustment_type IN('base_rate','percentage');
 
 -- Add include_in_cancel_fee to reservation_rates table
 ALTER TABLE reservation_rates
@@ -21,7 +21,7 @@ ALTER TABLE reservation_rates DISABLE TRIGGER log_reservation_rates_trigger;
 -- Batch update existing base_rate entries to set the new flag to TRUE
 UPDATE reservation_rates
 SET include_in_cancel_fee = TRUE
-WHERE adjustment_type = 'base_rate';
+WHERE adjustment_type  IN('base_rate','percentage');
 
 -- Enable the logging trigger after the batch update
 ALTER TABLE reservation_rates ENABLE TRIGGER log_reservation_rates_trigger;
