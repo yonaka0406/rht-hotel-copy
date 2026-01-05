b4f215ef4b21147fa43233238ba41a898681b810
almeida-rht <almeida.ped@redhorse-group.co.jp>
2026-01-05 09:24:55 +0900

    fix(adhoc): Ensure non-zero exit code on manual trigger failure
    - Track execution status in manual_trigger_sales_pdf.js.
    - Propagate exit code 1 if the job throws an exception.

--- api/adhoc_scripts/manual_trigger_sales_pdf.js
+++ api/adhoc_scripts/manual_trigger_sales_pdf.js
@@ -6,11 +6,13 @@ const { pool, prodPool } = require('../config/database');
 
 const run = async () => {
     logger.info('--- Starting Manual Trigger of Sales & Occ PDF Job ---');
+    let exitCode = 0;
     try {
         await runDailySalesOccPdfJob();
         logger.info('--- Job Execution Completed ---');
     } catch (error) {
         logger.error('--- Job Execution Failed ---', { message: error.message, stack: error.stack });
+        exitCode = 1;
     } finally {
         // Explicitly close database pools
         logger.info('Closing database connections...');
@@ -18,8 +20,8 @@ const run = async () => {
             pool.end(),
             prodPool.end()
         ]);
-        logger.info('Exiting process...');
-        process.exit(0);
+        logger.info(`Exiting process with code ${exitCode}...`);
+        process.exit(exitCode);
     }
 };
 
